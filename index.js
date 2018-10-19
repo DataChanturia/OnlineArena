d3121e67f0163f959713d2b0a25d970680a07bde
dchan13 <davidchanturia13@gmail.com>
2018-10-19 19:10:03 +0000

    challenges page styling [little bootstrap], new route, navigation anchor tagz, adding cover & name for challenge

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.set("view engine", "ejs");
 var challenges = [
         {name: "Beauty Challenge", coverImage: "https://marketplace.canva.com/MACQTpJkr5Q/1/0/thumbnail_large/canva-turquoise-pink-circles-beauty-parlor-cover-MACQTpJkr5Q.jpg"},
         {name: "Logo Challenge", coverImage: "https://cdn1.logocore.com/wp-content/uploads/2018/07/06171225/ckwdesigner-thirty-day-logo-challenge-2200x2200.jpg"},
+        {name: "Presidentz", coverImage: "https://mediaplanet.azureedge.net/images/318/53018/social-2016-trump-hil.jpg"},
+        {name: "Beauty Challenge", coverImage: "https://marketplace.canva.com/MACQTpJkr5Q/1/0/thumbnail_large/canva-turquoise-pink-circles-beauty-parlor-cover-MACQTpJkr5Q.jpg"},
+        {name: "Logo Challenge", coverImage: "https://cdn1.logocore.com/wp-content/uploads/2018/07/06171225/ckwdesigner-thirty-day-logo-challenge-2200x2200.jpg"},
         {name: "Presidentz", coverImage: "https://mediaplanet.azureedge.net/images/318/53018/social-2016-trump-hil.jpg"}];
         
 
