49a2ad86e5c595ee041ccdc2f67dea31e173e161
dchan13 <davidchanturia13@gmail.com>
2018-11-01 17:32:42 +0000

    added description of challenge + SHOW route + 'More' Info button to all challenges

--- index.js
+++ index.js
@@ -1,55 +1,81 @@
-
+//======================================== variable section
 var express = require("express");
 var app = express();
 
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
+
+//======================================== setup section
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb://localhost/online_arena");
 app.set("view engine", "ejs");
 
 
+//======================================== db/schema setup section
 //SCHEMA SETUP
 var challengeSchema = new mongoose.Schema({
     name: String,
-    coverImage: String
+    coverImage: String,
+    description: String
 });
 var Challenge = mongoose.model("Challenge", challengeSchema);
 
+
+//======================================== routing section
 // ROUTING GOES HERE
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.render("landing");
 });
 
-app.get("/challenges", function(req, res){
-    Challenge.find({}, function(err, allChallenges){
-       if(err){
-           console.log(err);
-       } else {
-           res.render("challenges", {challenges: allChallenges});
-       }
+// INDEX route -> shows all challenges
+app.get("/challenges", function(req, res) {
+    Challenge.find({}, function(err, allChallenges) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("index", { challenges: allChallenges });
+        }
     });
 });
 
-app.get("/challenges/new", function(req, res){
-    res.render("new");
-});
-
-app.post("/challenges", function(req, res){
+// CREATE route -> creates new challenge
+app.post("/challenges", function(req, res) {
     // get data from form + add to array
     var name = req.body.name;
     var coverImage = req.body.coverImage;
-    var newChallenge = {name: name, coverImage: coverImage};
-    Challenge.create(newChallenge, function(err, newlyCreated){
-        if(err){
+    var desc = req.body.description;
+    var newChallenge = { name: name, coverImage: coverImage, description: desc };
+    Challenge.create(newChallenge, function(err, newlyCreated) {
+        if (err) {
             console.log(err);
-        } else {
+        }
+        else {
             res.redirect("/challenges");
         }
     });
 });
 
+// NEW route -> shows form to create new challenge
+app.get("/challenges/new", function(req, res) {
+    res.render("new");
+});
+
+// SHOW route -> shows more info about challenge
+app.get("/challenges/:id", function(req, res) {
+    Challenge.findById(req.params.id, function(err, foundChallenge) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // render show template with that challenge
+            res.render("show", { challenge: foundChallenge });
+        }
+    });
+});
+
+//======================================== run section
 // SERVER START LISTENING TO IP/PORT
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("OnlineArena Server Started!");
